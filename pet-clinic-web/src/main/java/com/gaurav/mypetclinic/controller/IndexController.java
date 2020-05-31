775efd1fc63905e653a853ba0573662015d87a3b
Gauravkumar <gaurav.uottawa1094@gmail.com>
2020-05-31 19:42:36 -0400

    Added Index Controller and View

---
+++ pet-clinic-web/src/main/java/com/gaurav/mypetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.gaurav.mypetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String IndexController(){
+        return "index";
+    }
+}
